b174910c59864d2727323658b19074073ea49495
Duygu Algan <duygu.algan.esk26@gmail.com>
2022-07-02 19:04:47 +0300

    Uı design
    Uı design of coffee shop login and register screen

---
+++ CoffeeLogin/app/src/main/java/com/example/coffeelogin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.coffeelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import com.example.coffeelogin.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
+
